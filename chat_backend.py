8a695c203ce941d524f98854ec7f6535c81f3f29
Nazrin <u2000443@siswa.um.edu.my>
2024-06-17 23:08:06 +0800

    Removed unused imports, renamed variables, creating separate function to load model

--- chat_backend.py
+++ chat_backend.py
@@ -3,7 +3,7 @@ import random
 
 import torch
 from model import NeuralNetwork
-from utils import bag_of_words, tokenize, lemmatize
+from utils import bag_of_words, tokenize
 from nltk.corpus import stopwords
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -11,30 +11,32 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 with open('HEPintents.json', 'r') as intent_data:
     intents = json.load(intent_data)
 
-model_file = "model.pth"
-model = torch.load(model_file)
+def load_model(path: str):
+    model = torch.load(path)
 
-input_dim = model["input_dim"]
-fc_dim = model["fc_dim"]
-output_dim = model["output_dim"]
-all_words = model["all_words"]
-tags = model["tags"]
-model_state = model["model_state"]
+    input_dim = model["input_dim"]
+    fc_dim = model["fc_dim"]
+    output_dim = model["output_dim"]
+    all_words = model["all_words"]
+    tags = model["tags"]
+    model_state = model["model_state"]
 
-model = NeuralNetwork(input_dim, fc_dim, output_dim).to(device)
-model.load_state_dict(model_state)
-model.eval()
+    model = NeuralNetwork(input_dim, fc_dim, output_dim).to(device)
+    model.load_state_dict(model_state)
+    model.eval()
 
-chatbot_name = "HEP Chatbot"
-ignore_words = ["?", ".", "!", ","]
+    return {"all_words": all_words, "tags": tags, "model": model}.values()
 
 def chat(user_input):
     if(len(user_input) < 10):
         response = "Your query is too short. Please input a longer prompt for more contextual answers"
         return response
 
+    all_words, tags, model = load_model("model.pth")
+    punctuations = ["?", ".", "!", ","]
+
     sentence = [word for word in tokenize(user_input.lower()) if word not in stopwords.words('english')]
-    filtered_sentence = [word for word in sentence if word not in ignore_words]
+    filtered_sentence = [word for word in sentence if word not in punctuations]
     x = bag_of_words(filtered_sentence, all_words)
     x = x.reshape(1, x.shape[0])
     x = torch.from_numpy(x).to(device)
